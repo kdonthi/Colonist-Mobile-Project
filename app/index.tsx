54016d2fae250e4a8b17fc973f1d6e6f79ae9dc2
kdonthi <kaushikdr@gmail.com>
2025-02-01 10:56:58 -0800

    list working

---
+++ app/index.tsx
@@ -0,0 +1,115 @@
+import { Text, View, StyleSheet, FlatList } from "react-native";
+import { useState, useEffect } from 'react';
+import { Picker } from '@react-native-picker/picker';
+
+interface User {
+  createdAt: string;
+  userName: string;
+  country: string;
+  id: string;
+}
+
+enum SortBy {
+  None = "None",
+  CreationTimeAscending = "Creation Time (Ascending)",
+  CreationTimeDescending = "Creation Time (Descending)"
+}
+
+const PersonRow = (props: { user: User }) => (
+  <View>
+    <Text style={styles.text} id={props.user.id}>User: {props.user.userName} Country: {props.user.country} Created At: {props.user.createdAt}</Text>
+  </View>
+);
+
+export default function Index() {
+  const [data, setData] = useState<User[] | null>(null);
+  const [sortBy, setSortBy] = useState<SortBy>(SortBy.None);
+  const [countryFilter, setCountryFilter] = useState<string>("None");
+
+  useEffect(() => {
+    fetch("https://6799ee3d747b09cdcccd06bc.mockapi.io/api/v1/users")
+      .then(response => response.json())
+      .then((data: User[]) => setData(data));
+  }, []);
+
+  if (data !== null) {
+    let countries = data.map(user => user.country);
+    let uniqueCountries = [...new Set(countries)];
+
+    let filteredData = data;
+    if (countryFilter !== "None") {
+      filteredData = filterByCountry(filteredData, countryFilter);
+    }
+
+    if (sortBy === SortBy.CreationTimeAscending) {
+      filteredData = sortByCreationTime(filteredData, true);
+    } else if (sortBy === SortBy.CreationTimeDescending) {
+      filteredData = sortByCreationTime(filteredData, false);
+    }
+
+    return (
+      <View>
+        <Picker 
+          selectedValue={countryFilter}
+          onValueChange={setCountryFilter}
+        >
+          <Picker.Item label="None" value="None" />
+          {
+            uniqueCountries.map(c => {
+              return <Picker.Item label={c} value={c} />
+            })
+          }
+        </Picker>
+        <Picker 
+          selectedValue={sortBy}
+          onValueChange={setSortBy}
+        >
+          <Picker.Item label="None" value={SortBy.None} />
+          <Picker.Item label="Creation Time (Acending)" value={SortBy.CreationTimeAscending} />
+          <Picker.Item label="Creation Time (Descending)" value={SortBy.CreationTimeDescending} />
+        </Picker>
+        <FlatList 
+          data={filteredData}
+          renderItem={({ item }) => <PersonRow user={item} />}
+          keyExtractor={(item) => item.id}
+        />
+        
+      </View>
+    );
+  }
+  return null;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 25,
+    color: "black",
+  },
+  text2: {
+    fontSize: 25,
+    color: "green",
+  },
+  about: {
+    fontSize: 25,
+    color: "blue",
+    textDecorationLine: "underline",
+  }
+});
+
+
+function filterByCountry(data: User[], country: string) : User[] {
+  return data.filter(user => user.country === country);
+}
+
+function sortByCreationTime(data: User[], ascending: boolean) {
+  if (ascending) {
+    return data.sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime());
+  } else {
+    return data.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+  }
+}
